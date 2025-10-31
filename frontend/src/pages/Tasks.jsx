4bb768ecf12adc0fac49e57fa46705199dc78290
Vladdeek <vladryazanov2709@gmail.com>
2025-10-31 11:40:34 +0300

    fix webinars page for student

--- frontend/src/pages/Tasks.jsx
+++ frontend/src/pages/Tasks.jsx
@@ -23,6 +23,20 @@ import TiltedCard from '../components/ReactBits/TiledCard'
 import { Forbidden403, useError } from '../components/Errors'
 import axios from 'axios'
 
+const CreateBtn = ({ onClick, title, width = 'w-2/3', height = 'h-129' }) => {
+	return (
+		<button
+			onClick={onClick}
+			className={`flex flex-col ${width} max-md:w-full items-center justify-center border-1 border-[var(--middle)] text-[var(--middle)] rounded-xl group hover:border-[var(--hero-epta)] hover:text-[var(--hero-epta)] transition-all cursor-pointer max-md:h-75 max-md:mb-30 ${height}`}
+		>
+			<Blocks size={112} strokeWidth={0.5} />
+			<span className='text-base font-medium px-4 py-3 rounded-lg mt-4 transition-all'>
+				{title}
+			</span>
+		</button>
+	)
+}
+
 const CatalogS = ({ role }) => {
 	const [selected, setSelected] = useState(0)
 	const [selectedFilters, setSelectedFilters] = useState('all')
@@ -123,96 +137,59 @@ const CatalogS = ({ role }) => {
 	}, [role])
 
 	return (
-		<div
-			className={`${
-				courses?.length === 0 || courses?.length < 4 ? 'h-screen' : 'h-full'
-			} flex flex-col gap-4 py-[50px]`}
-		>
-			<div className='flex max-[874px]:gap-3 max-[874px]:flex-col-reverse justify-between'>
-				<div className='flex gap-4 max-lg:gap-2 h-12'>
-					{options.map(option => (
-						<RadioButton
-							key={option.value}
-							name='example'
-							value={option.value}
-							title={option.title}
-							icon={option.icon}
-							checked={selected === option.value}
-							onChange={() => NavigateTo(option.to, option.value)}
-						/>
-					))}
-				</div>
-				<div className='flex gap-4 max-lg:gap-2 h-12'>
-					<SearchInput />
-					{/* <FilterButton
-						option={[
-							'по статусу',
-							'по алфавиту',
-							'по дате создания',
-							'по хуйне ',
-						]}
-					/> */}
-				</div>
-			</div>
-			{location.pathname === '/catalogs/webinars' && (
-				<div className='flex gap-4 max-lg:gap-2 h-12'>
-					{filter?.map(option => (
-						<RadioButton
-							key={option.value}
-							name='filters'
-							value={option.value}
-							title={option.title}
-							icon={option.icon}
-							checked={selectedFilters === option.value}
-							onChange={() => setSelectedFilters(option.value)}
-						/>
-					))}
-				</div>
-			)}
-
-			{location.pathname === '/catalogs/courses' ? (
-				<div
-					className={`${
-						courses?.length === 0 || courses?.length < 5 ? 'h-screen' : 'h-full'
-					} flex flex-col gap-4 py-[50px]`}
-				>
-					<div className='grid 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 gap-4'>
-						{courses.map((course, index) => (
-							<motion.div
-								key={course.id}
-								initial={{ scale: 0.8, opacity: 0 }}
-								animate={{ scale: 1, opacity: 1 }}
-								transition={{
-									duration: 0.3,
-									delay: index * 0.1,
-									ease: 'easeOut',
-								}}
-							>
-								<CourseCard
-									title={course.name}
-									description={course.description}
-									img_path={`${FILE_API}${course.image_url}`}
-									status={course.status}
-									deadline={course.deadline}
-									to={`/course/${course.id}`}
+		<>
+			<div className={` flex flex-col gap-4 py-[50px]`}>
+				<div className='flex max-[874px]:gap-3 max-[874px]:flex-col-reverse justify-between'>
+					<div className='flex gap-4 max-lg:gap-2 h-12'>
+						{location.pathname !== '/catalog/all' &&
+							options?.map(option => (
+								<RadioButton
+									key={option.value}
+									name='example'
+									value={option.value}
+									title={option.title}
+									icon={option.icon}
+									checked={selected === option.value}
+									onChange={() => NavigateTo(option.to, option.value)}
 								/>
-							</motion.div>
-						))}
+							))}
+					</div>
+
+					<div className='flex gap-4 max-lg:gap-2 h-12'>
+						<SearchInput />
+						{/* <FilterButton
+									option={[
+										'по статусу',
+										'по алфавиту',
+										'по дате создания',
+										'по хуйне ',
+									]}
+								/> */}
 					</div>
 				</div>
-			) : (
-				location.pathname === '/catalogs/webinars' && (
-					<div
-						className={`${
-							webinars?.length === 0 || webinars?.length < 5
-								? 'h-screen'
-								: 'h-full'
-						} flex flex-col gap-4 py-[50px]`}
-					>
-						<div className='grid 2xl:grid-cols-5 xl:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-4'>
-							{webinars?.map((web, index) => (
+				{location.pathname === '/catalogt/webinars' && (
+					<div className='flex gap-4 max-lg:gap-2 h-12'>
+						{filter?.map(option => (
+							<RadioButton
+								key={option.value}
+								name='filters'
+								value={option.value}
+								title={option.title}
+								icon={option.icon}
+								checked={selectedFilters === option.value}
+								onChange={() => setSelectedFilters(option.value)}
+							/>
+						))}
+					</div>
+				)}
+
+				{location.pathname === '/catalogs/courses' ||
+				location.pathname === '/catalog/all' ? (
+					<div className={` flex flex-col gap-4 py-[50px]`}>
+						<div className='grid 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 gap-4'>
+							{courses?.map((course, index) => (
 								<motion.div
-									key={web.id}
+									key={course.id}
 									initial={{ scale: 0.8, opacity: 0 }}
 									animate={{ scale: 1, opacity: 1 }}
 									transition={{
@@ -221,21 +198,53 @@ const CatalogS = ({ role }) => {
 										ease: 'easeOut',
 									}}
 								>
-									<WebinarCard
-										title={web.name}
-										description={web.description}
-										img_path={`${FILE_API}${web.image_url}`}
-										start={web.start_date}
-										end={web.end_date}
-										to={web.link_url}
+									<CourseCard
+										title={course.name}
+										description={course.description}
+										img_path={`${FILE_API}${course.image_url}`}
+										status={course.status}
+										deadline={course.deadline}
+										to={
+											location.pathname === '/catalog/all'
+												? `/constructor/${course.id}`
+												: `/constructor/${course.id}`
+										}
 									/>
 								</motion.div>
 							))}
 						</div>
 					</div>
-				)
-			)}
-		</div>
+				) : (
+					location.pathname === '/catalogs/webinars' && (
+						<div className={` flex flex-col gap-4 py-[50px]`}>
+							<div className='grid 2xl:grid-cols-5 xl:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-4'>
+								{webinars?.map((web, index) => (
+									<motion.div
+										key={web.id}
+										initial={{ scale: 0.8, opacity: 0 }}
+										animate={{ scale: 1, opacity: 1 }}
+										transition={{
+											duration: 0.3,
+											delay: index * 0.1,
+											ease: 'easeOut',
+										}}
+									>
+										<WebinarCard
+											title={web.name}
+											description={web.description}
+											img_path={`${FILE_API}${web.image_url}`}
+											start={web.start_date}
+											end={web.end_date}
+											to={web.link_url}
+										/>
+									</motion.div>
+								))}
+							</div>
+						</div>
+					)
+				)}
+			</div>
+		</>
 	)
 }
 
