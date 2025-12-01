d665e53b8a13d88257a6f4de3af93dac2815772f
Vladdeek <vladryazanov2709@gmail.com>
2025-12-01 12:08:55 +0300

    schedule fix

--- frontend/src/pages/Schedule.jsx
+++ frontend/src/pages/Schedule.jsx
@@ -8,14 +8,16 @@ import {
 	startOfToday,
 	isSameDay,
 	set,
+	addWeeks,
 } from 'date-fns'
-import { ru } from 'date-fns/locale'
+import { da, ru } from 'date-fns/locale'
 import DirectionOfTraining from '../components/DirectionOfTraining'
-import { useEffect, useMemo, useState } from 'react'
+import { use, useEffect, useMemo, useState } from 'react'
 import api, { API } from '../API'
 import { getCookie } from '../TOKEN'
 import Loader from '../components/Loader'
 import axios from 'axios'
+import { ChevronLeft, ChevronRight } from 'lucide-react'
 
 const ScheduleCard1 = ({
 	lessonIndex,
@@ -117,15 +119,23 @@ const DayCard2 = ({ isSelected, day, weekDay, onClick }) => {
 	)
 }
 
-const Schedule1 = scheduleData => {
+const Schedule1 = ({
+	scheduleData,
+	Offset,
+	prev_active,
+	next_active,
+	selectedOffset,
+	WeekNumber,
+}) => {
+	const [weekOffset, setWeekOffset] = useState(selectedOffset)
 	const today = new Date()
-	const weekStart = startOfWeek(today, { weekStartsOn: 1 })
-	const weekDays = Array.from({ length: 6 }, (_, i) => addDays(weekStart, i))
-
-	const [scheduleData1, setScheduleData1] = useState()
-
-	const daySchedule = []
-
+	const weekStart = startOfWeek(addWeeks(today, weekOffset), {
+		weekStartsOn: 1,
+	})
+	const weekDays = useMemo(
+		() => Array.from({ length: 6 }, (_, i) => addDays(weekStart, i)),
+		[weekStart]
+	)
 	// Функция для проверки, является ли урок текущим
 	const isCurrentLesson = (lessonDate, lessonTime) => {
 		try {
@@ -144,57 +154,86 @@ const Schedule1 = scheduleData => {
 		}
 	}
 
+	useEffect(() => {
+		Offset?.(weekOffset)
+	}, [weekOffset])
+
 	return (
-		<div className=' grid grid-cols-6 gap-2'>
-			{weekDays.map((day, index) => {
-				const dateString = format(day, 'yyyy-MM-dd')
-				const daySchedule = scheduleData?.scheduleData[dateString] || []
-				const isCurrentDay = isToday(day)
+		<>
+			<div className='w-full flex justify-center items-center gap-5'>
+				<ChevronLeft
+					size={48}
+					onClick={() => prev_active && setWeekOffset(prev => prev - 1)}
+					className={`${!prev_active && 'opacity-50 cursor-not-allowed'}`}
+				/>
+				<p className='font-medium text-2xl mt-0.5'>Неделя {WeekNumber}</p>
+				<ChevronRight
+					size={48}
+					onClick={() => next_active && setWeekOffset(prev => prev + 1)}
+					className={`${!next_active && 'opacity-50 cursor-not-allowed'}`}
+				/>
+			</div>
+			<div className=' grid grid-cols-6 gap-2'>
+				{weekDays.map((day, index) => {
+					const dateString = format(day, 'yyyy-MM-dd')
+					const daySchedule = scheduleData?.[dateString] || []
+					const isCurrentDay = isToday(day)
 
-				return (
-					<div key={index} className='flex flex-col gap-4'>
-						<DayCard1
-							isCurrentDay={isCurrentDay}
-							day={format(day, 'd')}
-							month={format(day, 'MMMM', { locale: ru })}
-							weekDay={format(day, 'EEEEEE', { locale: ru }).toUpperCase()}
-						/>
-						<div className='flex flex-col gap-3'>
-							{daySchedule.length > 0 ? (
-								daySchedule.map((lesson, lessonIndex) => {
-									const isCurrent = isCurrentLesson(day, lesson.time_start)
+					return (
+						<div key={index} className='flex flex-col gap-4'>
+							<DayCard1
+								isCurrentDay={isCurrentDay}
+								day={format(day, 'd')}
+								month={format(day, 'MMMM', { locale: ru })}
+								weekDay={format(day, 'EEEEEE', { locale: ru }).toUpperCase()}
+							/>
+							<div className='flex flex-col gap-3'>
+								{daySchedule.length > 0 ? (
+									daySchedule.map((lesson, lessonIndex) => {
+										const isCurrent = isCurrentLesson(day, lesson.time_start)
 
-									return (
-										<ScheduleCard1
-											lessonIndex={lessonIndex}
-											isCurrent={isCurrent}
-											time_start={lesson.time_start}
-											time_end={lesson.time_end}
-											title={lesson.title}
-											description={lesson.description}
-										/>
-									)
-								})
-							) : (
-								<div className='bg-[var(--white)] rounded-xl shadow-[var(--shadow)] p-4 w-full flex items-center justify-center min-h-[120px]'>
-									<p className='text-[var(--middle)] text-center'>
-										Нет занятий
-									</p>
-								</div>
-							)}
+										return (
+											<ScheduleCard1
+												lessonIndex={lessonIndex}
+												isCurrent={isCurrent}
+												time_start={lesson.time_start}
+												time_end={lesson.time_end}
+												title={lesson.title}
+												description={lesson.description}
+											/>
+										)
+									})
+								) : (
+									<div className='bg-[var(--white)] rounded-xl shadow-[var(--shadow)] p-4 w-full flex items-center justify-center min-h-[120px]'>
+										<p className='text-[var(--middle)] text-center'>
+											Нет занятий
+										</p>
+									</div>
+								)}
+							</div>
 						</div>
-					</div>
-				)
-			})}
-		</div>
+					)
+				})}
+			</div>
+		</>
 	)
 }
 
-const Schedule2 = scheduleData => {
+const Schedule2 = ({
+	scheduleData,
+	Offset,
+	prev_active,
+	next_active,
+	selectedOffset,
+	WeekNumber,
+}) => {
+	const [weekOffset, setWeekOffset] = useState(selectedOffset)
 	const today = startOfToday()
 
 	// 🔥 Начало текущей недели (понедельник)
-	const weekStart = startOfWeek(today, { weekStartsOn: 1 })
+	const weekStart = startOfWeek(addWeeks(today, weekOffset), {
+		weekStartsOn: 1,
+	})
 
 	// 🔥 Генерация именно текущей недели (6 дней, как у Schedule1)
 	const weekDays = useMemo(
@@ -225,109 +264,129 @@ const Schedule2 = scheduleData => {
 	}
 
 	const selectedDateString = format(selectedDay, 'yyyy-MM-dd')
-	const selectedDaySchedule =
-		scheduleData?.scheduleData[selectedDateString] || []
+	const selectedDaySchedule = scheduleData?.[selectedDateString] || []
+
+	useEffect(() => {
+		Offset?.(weekOffset)
+	}, [weekOffset])
+
+	console.log(prev_active, next_active)
 
 	return (
-		<div className='grid grid-cols-6 gap-2'>
-			{/* Дни недели */}
-			{weekDays.map((day, index) => {
-				const isCurrentDay = isToday(day)
-				const isSelected = isSameDay(day, selectedDay)
-
-				return (
-					<div key={index} className='flex flex-col gap-4'>
-						<DayCard2
-							isCurrentDay={isCurrentDay}
-							isSelected={isSelected}
-							onClick={() => setSelectedDay(day)}
-							day={format(day, 'd')}
-							month={format(day, 'MMMM', { locale: ru })}
-							weekDay={format(day, 'EEEEEE', { locale: ru }).toUpperCase()}
-						/>
-					</div>
-				)
-			})}
-
-			{/* Расписание выбранного дня */}
-			<div className='col-span-6 flex flex-col gap-3 mt-1'>
-				{selectedDaySchedule?.length > 0 ? (
-					selectedDaySchedule.map((lesson, lessonIndex) => {
-						const isCurrent = isCurrentLesson(selectedDay, lesson?.time_start)
-
-						return (
-							<ScheduleCard2
-								key={lessonIndex}
-								lessonIndex={lessonIndex}
-								isCurrent={isCurrent}
-								time_start={lesson.time_start}
-								time_end={lesson.time_end}
-								title={lesson.title}
-								description={lesson.description}
-							/>
-						)
-					})
-				) : (
-					<div className='bg-[var(--white)] rounded-xl shadow-[var(--shadow)] p-4 w-full flex items-center justify-center min-h-[120px]'>
-						<p className='text-[var(--middle)] text-center'>Нет занятий</p>
-					</div>
-				)}
+		<>
+			<div className='w-full flex justify-between items-center mb-3'>
+				<ChevronLeft
+					size={48}
+					onClick={() => prev_active && setWeekOffset(prev => prev - 1)}
+					className={`${!prev_active && 'opacity-50 cursor-not-allowed'}`}
+				/>
+				<p className='font-medium text-2xl mt-0.5'>Неделя {WeekNumber}</p>
+				<ChevronRight
+					size={48}
+					onClick={() => next_active && setWeekOffset(prev => prev + 1)}
+					className={`${!next_active && 'opacity-50 cursor-not-allowed'}`}
+				/>
 			</div>
-		</div>
+			<div className='grid grid-cols-6 gap-2'>
+				{/* Дни недели */}
+				{weekDays.map((day, index) => {
+					const isCurrentDay = isToday(day)
+					const isSelected = isSameDay(day, selectedDay)
+
+					return (
+						<div key={index} className='flex flex-col gap-4'>
+							<DayCard2
+								isCurrentDay={isCurrentDay}
+								isSelected={isSelected}
+								onClick={() => setSelectedDay(day)}
+								day={format(day, 'd')}
+								month={format(day, 'MMMM', { locale: ru })}
+								weekDay={format(day, 'EEEEEE', { locale: ru }).toUpperCase()}
+							/>
+						</div>
+					)
+				})}
+
+				{/* Расписание выбранного дня */}
+				<div className='col-span-6 flex flex-col gap-3 mt-1'>
+					{selectedDaySchedule?.length > 0 ? (
+						selectedDaySchedule.map((lesson, lessonIndex) => {
+							const isCurrent = isCurrentLesson(selectedDay, lesson?.time_start)
+
+							return (
+								<ScheduleCard2
+									key={lessonIndex}
+									lessonIndex={lessonIndex}
+									isCurrent={isCurrent}
+									time_start={lesson.time_start}
+									time_end={lesson.time_end}
+									title={lesson.title}
+									description={lesson.description}
+								/>
+							)
+						})
+					) : (
+						<div className='bg-[var(--white)] rounded-xl shadow-[var(--shadow)] p-4 w-full flex items-center justify-center min-h-[120px]'>
+							<p className='text-[var(--middle)] text-center'>Нет занятий</p>
+						</div>
+					)}
+				</div>
+			</div>
+		</>
 	)
 }
 
 const SchedulePage = () => {
 	const [scheduleData, setScheduleData] = useState({})
+	const [allScheduleData, setAllScheduleData] = useState({})
 	const [loading, setLoading] = useState(false)
 	const [error, setError] = useState(null)
+	const [current_week, setCurrentWeek] = useState(0)
 
-	useEffect(() => {
-		console.log('Начинаем загрузку расписания для группы')
-		const loadSchedule = async () => {
-			setLoading(true)
-			console.log('1) начало')
-			try {
-				const res = await api.get(`${API}/schedule-lessons`, {
+	const loadSchedule = async () => {
+		setLoading(true)
+
+		try {
+			const res = await api.get(
+				`${API}/schedule-lessons?week_offset=${current_week}`,
+				{
 					withCredentials: true,
 					headers: {
 						'X-CSRF-TOKEN': getCookie('csrftoken'),
 					},
-				})
+				}
+			)
 
-				const data = res.data
+			const data = res.data
 
-				console.log('2) данные: ', data)
+			setAllScheduleData(data)
 
-				const normalized = {}
+			const normalized = {}
 
-				Object.entries(data).forEach(([date, lessons]) => {
-					const shortDate = date.split('T')[0] // 2025-11-17
+			Object.entries(data?.schedule).forEach(([date, lessons]) => {
+				const shortDate = date.split('T')[0] // 2025-11-17
 
-					normalized[shortDate] = lessons.map(lesson => ({
-						time_start: lesson.time_start.slice(0, 5), // 08:00
-						time_end: lesson.time_end.slice(0, 5), // 09:20
-						title: `${lesson.subject} (${lesson.lesson_type})`,
-						description: `${lesson.teacher_name}, ауд. ${lesson.auditory_name}`,
-						raw: lesson,
-					}))
-				})
+				normalized[shortDate] = lessons.map(lesson => ({
+					time_start: lesson.time_start.slice(0, 5), // 08:00
+					time_end: lesson.time_end.slice(0, 5), // 09:20
+					title: `${lesson.subject} (${lesson.lesson_type})`,
+					description: `${lesson.teacher_name}, ауд. ${lesson.auditory_name}`,
+					raw: lesson,
+				}))
+			})
 
-				console.log('3) нормализованные данные: ', normalized)
-
-				setScheduleData(normalized)
-				setLoading(false)
-
-				console.log('4) конец загрузки')
-			} catch (e) {
-				console.error(e)
-				setError(e.response ? String(e.response.status) : '500')
-				setLoading(false)
-			}
+			setScheduleData(normalized)
+			setLoading(false)
+		} catch (e) {
+			console.error(e)
+			setError(e.response ? String(e.response.status) : '500')
+			setLoading(false)
 		}
+	}
 
+	useEffect(() => {
 		loadSchedule()
-	}, [])
+	}, [current_week])
 
 	return (
 		<div className='flex flex-col gap-2 p-4 h-screen'>
@@ -339,10 +398,24 @@ const SchedulePage = () => {
 			) : (
 				<>
 					<div className='lg:hidden'>
-						<Schedule2 scheduleData={scheduleData} />
+						<Schedule2
+							scheduleData={scheduleData}
+							Offset={data => setCurrentWeek(data)}
+							prev_active={allScheduleData?.has_prev}
+							next_active={allScheduleData?.has_next}
+							selectedOffset={current_week}
+							WeekNumber={allScheduleData?.current_week_number}
+						/>
 					</div>
 					<div className='max-lg:hidden'>
-						<Schedule1 scheduleData={scheduleData} />
+						<Schedule1
+							scheduleData={scheduleData}
+							Offset={data => setCurrentWeek(data)}
+							prev_active={allScheduleData?.has_prev}
+							next_active={allScheduleData?.has_next}
+							selectedOffset={current_week}
+							WeekNumber={allScheduleData?.current_week_number}
+						/>
 					</div>
 				</>
 			)}
